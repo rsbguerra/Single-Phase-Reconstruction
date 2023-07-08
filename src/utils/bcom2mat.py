ac945ac66d4edeb71d305e314aeca2002d6dd1f4
Raquel Guerra <rsbguerra@tutamail.com>
2023-07-08 21:40:00 +0100

    Accept arguments by cli

--- src/utils/bcom2mat.py
+++ src/utils/bcom2mat.py
@@ -1,14 +1,24 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import re
-from hdf5storage import savemat
-# from scipy.io import savemat
+from os.path import exists
+from os import getcwd
+from sys import argv
 
+import matplotlib.pyplot as plt
+import numpy as np
 import PIL
+from hdf5storage import savemat
+
 PIL.Image.MAX_IMAGE_PIXELS = 268435460
 
+WORKING_DIR = re.search('(^\S+\/Single\-Phase\-Reconstruction)',getcwd()).group(1)
+OUT_DIR = WORKING_DIR + '/data/input/holograms/'
+
 def _convert_bmp(bmp_dir:str, output_dir:str):
-    hol_name = re.search('bcom\/(\S+)-AP\/', bmp_dir).group(1)
+    # Matches any word characters preceded by '-AP'
+    hol_name = re.search('\w+(?=-AP)', bmp_dir).group(0)
+
+    if not bmp_dir[-1] == '/':
+        bmp_dir = bmp_dir + '/'
 
     am_path = f'{bmp_dir}{hol_name}_ampli.bmp'
     ph_path = f'{bmp_dir}{hol_name}_phase.bmp'
@@ -21,13 +31,17 @@ def _convert_bmp(bmp_dir:str, output_dir:str):
     mat_file = f'{output_dir}{hol_name}.mat'
     savemat(mat_file, mdict={'data': data}, do_compression=True, format='7.3')
 
-def convert():
-    holo_dirs = [
-        '/mnt/data/Holograms/bcom/deepDices2k-AP/',
-        '/mnt/data/Holograms/bcom/deepDices16k-AP/'
-    ]
-
+def convert(holo_dirs):
     for dir in holo_dirs:
-        _convert_bmp(dir, dir)
+        if exists(dir):
+            _convert_bmp(dir, OUT_DIR)
+        else:
+            print(f'[WARNING] Unable to find {dir}.')
 
-convert()
+# convert()
+
+if __name__ == "__main__":
+    if len(argv) > 1:
+        convert(argv[1:])
+    else:
+        print('[ERROR] No arguments passed.')
