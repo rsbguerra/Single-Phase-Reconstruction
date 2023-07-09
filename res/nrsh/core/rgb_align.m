29bf4c461c47751a7bc9fe0b2611d360e836b89d
Raquel Guerra <rsbguerra@tutamail.com>
2023-07-09 01:35:50 +0100

    Create NRSH licence file

--- res/nrsh/core/rgb_align.m
+++ res/nrsh/core/rgb_align.m
@@ -12,52 +12,7 @@ function [recons_ali] = rgb_align(recons, info)
     %                        channels aligned. The dimension is equal to that
     %                        specified in the configuration file.
     %
-    %-------------------------------------------------------------------------
-    % Copyright(c) 2019
-    % University of Cagliari
-    % Department of Electrical and Electronic Engineering
-    % Italy
-    % All Rights Reserved.
-    %-------------------------------------------------------------------------
-    %
-    % The University of Cagliari - Department of Electrical and Electronic
-    % Engineering hereby grants to ISO/IEC JTC1 SC29 WG1
-    % (JPEG Committee) and each Member of ISO/IEC JTC1 SC29 WG1 (JPEG
-    % Committee) who participate in the Working Group dedicated to the
-    % standardization of JPEG Pleno, a non-exclusive, nontransferable,
-    % worldwide, license under "University of Cagliari - Department of
-    % Electrical and Electronic Engineering" copyrights
-    % in this software to reproduce, distribute, display, perform and
-    % create derivative works for the sole and exclusive purposes of
-    % creating a hologram reconstruction software in the frame of
-    % the JPEG Pleno standard.
-    %
-    % Modifications to this code shall be clearly indicated and
-    % identified by the relevant copyright notice(s) of the party
-    % generating these changes and/or derivative works.
-    %
-    % Nothing contained in this software shall, except as herein
-    % expressly provided, be construed as conferring by implication,
-    % estoppel or otherwise, any license or right under (i) any existing
-    % or later issuing patent, whether or not the use of information in
-    % this software necessarily employs an invention of any existing or
-    % later issued patent, (ii) any copyright, (iii) any trademark, or
-    % (iv) any other intellectual property right.
-    %
-    % THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS "AS IS" AND
-    % ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-    % TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-    % PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-    % COPYRIGHT OWNER BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-    % SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-    % LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
-    % USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
-    % AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-    % LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
-    % IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
-    % THE POSSIBILITY OF SUCH DAMAGE.
-    %
-    %-------------------------------------------------------------------------
+
 
     %recons=uint8(recons.*255);%recons should be the out. of the stretch.
 
@@ -67,11 +22,11 @@ function [recons_ali] = rgb_align(recons, info)
     shift_yx_R = (info.shift_yx_r) .* (recons_img_size) ./ ...
         (info.wlen(1)) / (info.ref_wave_rad) * ...
         (info.pixel_pitch);
-    
+
     shift_yx_G = (info.shift_yx_g) .* (recons_img_size) ./ ...
         (info.wlen(2)) / (info.ref_wave_rad) * ...
         (info.pixel_pitch);
-    
+
     %NOT USED, for now:
     %shift_yx_B=(rec_par_cfg.shift_yx_b) .* (rec_par_cfg.out_size) ./...
     %            (rec_par_cfg.wlen(3)) / (rec_par_cfg.ref_wave_rad) *...
--- res/nrsh/core/wut_filter.m
+++ res/nrsh/core/wut_filter.m
@@ -9,52 +9,7 @@ function [hol_rendered] = wut_filter(hologram, info)
     %   Output:
     %    hol_rendered      - filtered reconstruction.
     %
-    %-------------------------------------------------------------------------
-    % Copyright(c) 2019
-    % University of Cagliari
-    % Department of Electrical and Electronic Engineering
-    % Italy
-    % All Rights Reserved.
-    %-------------------------------------------------------------------------
-    %
-    % The University of Cagliari - Department of Electrical and Electronic
-    % Engineering hereby grants to ISO/IEC JTC1 SC29 WG1
-    % (JPEG Committee) and each Member of ISO/IEC JTC1 SC29 WG1 (JPEG
-    % Committee) who participate in the Working Group dedicated to the
-    % standardization of JPEG Pleno, a non-exclusive, nontransferable,
-    % worldwide, license under "University of Cagliari - Department of
-    % Electrical and Electronic Engineering" copyrights
-    % in this software to reproduce, distribute, display, perform and
-    % create derivative works for the sole and exclusive purposes of
-    % creating a hologram reconstruction software in the frame of
-    % the JPEG Pleno standard.
-    %
-    % Modifications to this code shall be clearly indicated and
-    % identified by the relevant copyright notice(s) of the party
-    % generating these changes and/or derivative works.
-    %
-    % Nothing contained in this software shall, except as herein
-    % expressly provided, be construed as conferring by implication,
-    % estoppel or otherwise, any license or right under (i) any existing
-    % or later issuing patent, whether or not the use of information in
-    % this software necessarily employs an invention of any existing or
-    % later issued patent, (ii) any copyright, (iii) any trademark, or
-    % (iv) any other intellectual property right.
-    %
-    % THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS "AS IS" AND
-    % ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-    % TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-    % PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-    % COPYRIGHT OWNER BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-    % SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-    % LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
-    % USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
-    % AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-    % LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
-    % IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
-    % THE POSSIBILITY OF SUCH DAMAGE.
-    %
-    %-------------------------------------------------------------------------
+
 
     %% WUT_DISPLAY ONLY OPERATIONS
     depth = size(hologram, 3);
