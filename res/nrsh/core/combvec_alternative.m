00fd382b9f2975706e74c0516b1e24bbf090f5b4
Raquel Guerra <rsbguerra@tutamail.com>
2023-07-09 01:35:50 +0100

    Create NRSH licence file

--- res/nrsh/core/combvec_alternative.m
+++ res/nrsh/core/combvec_alternative.m
@@ -5,52 +5,7 @@ function [comb] = combvec_alternative(rec_dists, h_pos, v_pos, ap_sizes)
     %ap_sizes) in order to get all the possible combinations. The output is not
     %the inputs combined, as in previous verisions of NRSH (1.1.1 or below).
     %
-    %-------------------------------------------------------------------------
-    % Copyright(c) 2019
-    % University of Cagliari
-    % Department of Electrical and Electronic Engineering
-    % Italy
-    % All Rights Reserved.
-    %-------------------------------------------------------------------------
-    %
-    % The University of Cagliari - Department of Electrical and Electronic
-    % Engineering hereby grants to ISO/IEC JTC1 SC29 WG1
-    % (JPEG Committee) and each Member of ISO/IEC JTC1 SC29 WG1 (JPEG
-    % Committee) who participate in the Working Group dedicated to the
-    % standardization of JPEG Pleno, a non-exclusive, nontransferable,
-    % worldwide, license under "University of Cagliari - Department of
-    % Electrical and Electronic Engineering" copyrights
-    % in this software to reproduce, distribute, display, perform and
-    % create derivative works for the sole and exclusive purposes of
-    % creating a hologram reconstruction software in the frame of
-    % the JPEG Pleno standard.
-    %
-    % Modifications to this code shall be clearly indicated and
-    % identified by the relevant copyright notice(s) of the party
-    % generating these changes and/or derivative works.
-    %
-    % Nothing contained in this software shall, except as herein
-    % expressly provided, be construed as conferring by implication,
-    % estoppel or otherwise, any license or right under (i) any existing
-    % or later issuing patent, whether or not the use of information in
-    % this software necessarily employs an invention of any existing or
-    % later issued patent, (ii) any copyright, (iii) any trademark, or
-    % (iv) any other intellectual property right.
-    %
-    % THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS "AS IS" AND
-    % ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-    % TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-    % PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-    % COPYRIGHT OWNER BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-    % SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-    % LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
-    % USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
-    % AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-    % LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
-    % IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
-    % THE POSSIBILITY OF SUCH DAMAGE.
-    %
-    %-------------------------------------------------------------------------
+
     comb = {1:size(rec_dists, 2), 1:size(h_pos, 2), 1:size(v_pos, 2), 1:size(ap_sizes, 2)};
     [comb{:}] = ndgrid(comb{:});
     n = length(comb);
