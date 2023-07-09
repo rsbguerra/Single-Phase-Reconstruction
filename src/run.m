6bf9b2ab7e6274fda3663183b44105002ccef7bb
Raquel Guerra <rsbguerra@tutamail.com>
2023-07-09 01:39:18 +0100

    Reconstruct DeepDices8k4k with NRH
    Opens issue #2

--- src/run.m
+++ src/run.m
@@ -7,7 +7,6 @@ add_paths(curr_dir, ["res/nrsh/" ...
                      "res/nrsh/core/" ...
                      "res/nrsh/core/WUT_lib/" ...
                      "src/" ...
-                     "src/holo_config/" ...
                      "src/reconstruction/" ...
                      "src/utils/"
                      ])
@@ -16,11 +15,13 @@ add_paths(curr_dir, ["res/nrsh/" ...
 hologram_name = 'DeepDices8K4K';
 
 rec_dists = 0.331;
-h_pos = [-1 0 1];
-v_pos = 0;
+h_pos = [-0.5 0.5];
+v_pos = [-0.75 0.75];
 channel = 1;
 
 % Testing if the reconstruction function is being used correctly
 % After testing, single_phase_rec will be used
-reconstruct(hologram_name, rec_dists, h_pos, v_pos);
+
+reconstruct_nrsh(hologram_name, rec_dists, h_pos, v_pos)
+% reconstruct(hologram_name, rec_dists, h_pos, v_pos);
 % single_phase_rec(hologram_name, rec_dists, h_pos, v_pos, channel);
