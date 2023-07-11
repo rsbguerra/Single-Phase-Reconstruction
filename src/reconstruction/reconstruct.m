c4adffbabdcb4c6cce1f92ed4e8430c0cb9937fe
Raquel Guerra <rsbguerra@tutamail.com>
2023-07-11 15:47:04 +0100

    Use load hologram function to declutter code

--- src/reconstruction/reconstruct.m
+++ src/reconstruction/reconstruct.m
@@ -1,52 +1,11 @@
 function reconstruct(hologram_name, rec_dist, h_pos, v_pos)
     curr_dir = working_dir();
-    config_dir = fullfile(curr_dir, 'data/config/single_phase_config/');
+    figure_dir = fullfile(curr_dir, 'data/output/reconstruction', hologram_name);
 
     %% Load config
     disp('Loading config');
 
-    switch hologram_name
-
-        case 'Biplane16k'
-            load([config_dir 'Biplane16k_config.mat'], 'info', 'hologram_path');
-            load(hologram_path, 'CGH');
-            original_hologram = CGH.Hol;
-            clearvars CGH
-
-        case 'CGH_Venus'
-            load([config_dir 'CGH_Venus_config.mat'], 'info', 'hologram_path');
-            load(hologram_path, 'CGH');
-            original_hologram = CGH.Hol;
-            clearvars CGH
-
-        case 'DeepDices16K'
-            load([config_dir 'DeepDices16K_config.mat'], 'info', 'hologram_path');
-            load(hologram_path, 'data');
-            original_hologram = single(data);
-            clearvars data
-
-        case 'DeepDices2K'
-            load([config_dir 'DeepDices2K_config.mat'], 'info', 'hologram_path');
-            load(hologram_path, 'data');
-            original_hologram = single(data);
-            clearvars data
-
-        case 'DeepDices8K4K'
-            load([config_dir 'DeepDices8K4K_config.mat'], 'info', 'hologram_path');
-            load(hologram_path, 'data');
-            original_hologram = single(data);
-            clearvars data
-
-        case 'Lowiczanka_Doll'
-            wut_single_ph_rec(rec_dist, h_pos, v_pos, channel);
-            return
-
-        otherwise
-            warning('Hologram not recognized. Exiting script.')
-            return
-    end
-
-    figure_dir = fullfile(curr_dir, 'data/output/reconstruction', hologram_name);
+    [original_hologram, info] = load_hologram(hologram_name);
 
     if ~exist(figure_dir, "dir")
         mkdir(figure_dir);
