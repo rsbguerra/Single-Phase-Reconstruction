02be3ad27f21d566814e5e87e281ad73411958a9
rsbguerra <rsbguerra@tutamail.com>
2023-07-16 17:25:08 +0100

    Add case tests

--- tests/run_nrsh_test.m
+++ tests/run_nrsh_test.m
@@ -12,24 +12,23 @@ add_paths(parent_dir, ["res/nrsh/" ...
                      ])
 log_dir = "nrsh/logs/";
 
-folder_info = dir('nrsh/*.m');
+folder_info = dir('nrsh/*_2016x59394.m');
 %folder_info = dir('nrsh/Lowiczanka_Doll*.m');
 
 for i = 1:size(folder_info, 1) % allFiles has one row per file, so loop over those
     script_name = folder_info(i).name;
     script = sprintf("%stests/nrsh/%s",parent_dir, script_name);
-    
+
     log_dir = "nrsh/logs/"+script_name(1:end-2);
     time = string(datetime('now', 'Format', 'dd-MMM-y_HH:mm:ss.SSS'));
     log_path = sprintf("%s/%s.log", log_dir, time);
 
-    
     if ~exist(log_dir, 'dir')
         mkdir(log_dir);
     end
-    
+
     diary(convertStringsToChars(log_path))
-    
+
     try
         fprintf(1, "\n[TEST] Running %s test script.\n", script);
         run(convertStringsToChars(script));
@@ -41,4 +40,5 @@ for i = 1:size(folder_info, 1) % allFiles has one row per file, so loop over tho
         disp(getReport(error,'extended','hyperlinks','off'))
         diary off
     end
+
 end
