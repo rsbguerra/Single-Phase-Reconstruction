24f0ec8d731a7857af32d6cd1a5746bfc69c08e5
rsbguerra <rsbguerra@tutamail.com>
2023-08-01 18:35:08 +0100

    Fix function name
    - Rename function from single_phase_rec to reconstruct
    - Cycle through input channels

--- src/reconstruction/reconstruct.m
+++ src/reconstruction/reconstruct.m
@@ -1,4 +1,4 @@
-function single_phase_rec(hologram_name, rec_dist, h_pos, v_pos, ap_sizes, channel)
+function reconstruct(hologram_name, rec_dist, h_pos, v_pos, ap_sizes, channel)
 
     if hologram_name == "Lowiczanka_Doll"
         wut_single_ph_rec(rec_dist, h_pos, v_pos, channel)
@@ -10,47 +10,50 @@ function single_phase_rec(hologram_name, rec_dist, h_pos, v_pos, ap_sizes, chann
     % Load config
     disp('Loading config');
 
-    [single_ph_holo, info] = load_hologram(hologram_name, channel);
-
     figure_dir = fullfile(curr_dir, 'data/output/single_phase_fig', hologram_name);
 
     if ~exist(figure_dir, "dir")
         mkdir(figure_dir);
     end
 
-    for d = rec_dist
+    for c = channel
+        [single_ph_holo, info] = load_hologram(hologram_name, channel);
 
-        for a = ap_sizes
-            ap_size = a{1};
+        for d = rec_dist
 
-            for h = h_pos
+            for a = ap_sizes
+                ap_size = a{1};
 
-                for v = v_pos
+                for h = h_pos
 
-                    % Apperture application
-                    [single_ph_holo] = aperture(single_ph_holo, ...
-                        info.dataset, ...
-                        info.rec_par_cfg, ...
-                        d, h, v, ...
-                        ap_size(1));
+                    for v = v_pos
 
-                    hol_rendered_forward = num_rec(single_ph_holo, info.dataset, info.rec_par_cfg, d, info.direction);
-                    holo_abs = abs(hol_rendered_forward);
+                        % Apperture application
+                        [single_ph_holo] = aperture(single_ph_holo, ...
+                            info.dataset, ...
+                            info.rec_par_cfg, ...
+                            d, h, v, ...
+                            ap_size(1));
 
-                    if channel
-                        ch_str = channel2string(channel);
-                    else
-                        ch_str = 'rgb';
-                    end
+                        hol_rendered_forward = num_rec(single_ph_holo, info.dataset, info.rec_par_cfg, d, info.direction);
+                        holo_abs = abs(hol_rendered_forward);
 
-                    figure_name = sprintf('%s_%s_%g_[%dx%d]_[%gx%g].png', hologram_name, ch_str, d, ap_size(1), ap_size(2), h, v);
-                    figure_path = fullfile(figure_dir, figure_name);
+                        if channel
+                            ch_str = channel2string(channel);
+                        else
+                            ch_str = 'rgb';
+                        end
 
-                    try
-                        imwrite(holo_abs, figure_path);
-                    catch
-                        holo_abs = imresize(holo_abs, 0.25);
-                        imwrite(holo_abs, figure_path);
+                        figure_name = sprintf('%s_%s_%g_[%dx%d]_[%gx%g].png', hologram_name, ch_str, d, ap_size(1), ap_size(2), h, v);
+                        figure_path = fullfile(figure_dir, figure_name);
+
+                        try
+                            imwrite(holo_abs, figure_path);
+                        catch
+                            holo_abs = imresize(holo_abs, 0.25);
+                            imwrite(holo_abs, figure_path);
+                        end
+
                     end
 
                 end
